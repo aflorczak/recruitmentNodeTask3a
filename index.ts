a4f095f5ecd6d21cce35b01654a7a85f20e24931
Adrian Florczak <adrian.florczak92@gmail.com>
2022-05-19 17:49:59 +0200

    routing is ready

--- index.ts
+++ index.ts
@@ -7,7 +7,24 @@ const app: Express = express();
 app.set('PORT', process.env.PORT || 80);
 app.set('NODE_ENV', process.env.NODE_ENV || 'prod');
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/favorites/:id/file', (req: Request, res: Response) => {
+    res.send('/favorites/:id/file');
+});
+
+app.get('/favorites/:id', (req: Request, res: Response) => {
+    res.send('/favorites/:id');
+});
+
+app.get('/favorites', (req: Request, res: Response) => {
+    res.send("favorites get");
+});
+
+app.post('/favorites', (req: Request, res: Response) => {
+    res.send('favorites post');
+});
+
+
+app.get('/films', (req: Request, res: Response) => {
     res.send('ok');
 });
 
